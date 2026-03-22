5c9d7d9adc34c45bae82ec90b1af4cd738a85309
nysthretha <ahmetatsiz@gmail.com>
2026-03-22 22:51:05 +0300

    Build site into docs directory for GitHub Pages, add configurable basepath

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from markdowntohtml import markdown_to_html_node
 from extracttitle import extract_title
@@ -19,7 +20,7 @@ def copy_static(src, dst):
             copy_static(src_path, dst_path)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -28,25 +29,27 @@ def generate_page(from_path, template_path, dest_path):
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    page = page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(page)
 
 
-def generate_pages_recursive(content_dir, template_path, dest_dir):
+def generate_pages_recursive(content_dir, template_path, dest_dir, basepath):
     for item in os.listdir(content_dir):
         src_path = os.path.join(content_dir, item)
         if os.path.isfile(src_path):
             if src_path.endswith(".md"):
                 dest_path = os.path.join(dest_dir, item.replace(".md", ".html"))
-                generate_page(src_path, template_path, dest_path)
+                generate_page(src_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(src_path, template_path, os.path.join(dest_dir, item))
+            generate_pages_recursive(src_path, template_path, os.path.join(dest_dir, item), basepath)
 
 
 def main():
-    copy_static("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_static("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 main()
